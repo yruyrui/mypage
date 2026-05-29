11cb73888614718622b52bdbe3f94d688f155d39
yruyrui <saito.ryuto.ss8@naist.ac.jp>
2026-05-30 07:08:25 +0900

    init

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [icon()]
+});
